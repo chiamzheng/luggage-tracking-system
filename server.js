305a0c97724663833f7355f0325a3f41d20e75b5
chiamzheng <127980671+chiamzheng@users.noreply.github.com>
2024-03-21 04:37:46 +0800

    Update server.js
    renamed

--- server.js
+++ server.js
@@ -152,14 +152,14 @@ app.get('/', (req, res) => {
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flight Tracking</title>
+    <title>Luggage Tracking</title>
     <link rel="stylesheet" href="/styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   </head>
   <body>
     <div class="container">
-      <h1>Welcome to Flight Tracking App</h1>
+      <h1>Welcome to the Luggage Tracking App</h1>
       <p>Please enter your Passenger ID:</p>
       <form id="passengerForm">
         <input type="text" id="passengerId" name="passengerId" placeholder="Enter Passenger ID">
